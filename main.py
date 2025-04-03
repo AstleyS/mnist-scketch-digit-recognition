13471b48621c5014b4e65d029ede7c7881889d66
Astley Santos <astley_394@hotmail.com>
2025-04-03 13:15:22 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# Import Pytorch
+import torch
+from torch import nn
+
+# Import torchvision
+import torchvision.datasets
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+
+print(f"PyTorch version: {torch.__version__}\ntorchvision version: {torchvision.__version__}")
