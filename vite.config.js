3a8c5d85901c8c684c24fa87153af1f03bbc9021
Astley Santos <astley_394@hotmail.com>
2025-05-28 01:54:47 +0200

    fix base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/mnist-sketch-digit-recognition/',
   plugins: [
     react(),
     tailwindcss(),
